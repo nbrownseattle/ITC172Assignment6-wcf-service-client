3a455b223abfb5ddbe2b8e00e2163f6d23b681a9
nbrownseattle <nbrown28@seattlecentral.edu>
2017-03-06 13:40:10 -0800

    Adds files for assignment 6
    ITC 172 WCF Service Client

---
+++ CommunityAssistClient/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+        CAServiceReference.CAServiceClient cas = 
+            new CAServiceReference.CAServiceClient();
+        int key = cas.PersonLogin(UserNameTextBox.Text,PasswordTextBox.Text);
+        if(key != 0)//if key is not equal to zero, then legit key. Otherwise login failed
+        {
+            Session["userkey"] = key;
+            Response.Redirect("GrantPage.aspx");
+        }
+        else
+        {
+            ResultLabel.Text = "Login failed";
+        }
+
+    }
+}
